a3a37d9f902dbf64f67e91f30887e4370afe5228
afalcozvigne <a.falcozvigne@epsi.fr>
2016-03-03 16:14:48 +0100

    03-03-2016 16h14

--- WaveAttitudeDLL/WaveAttitudeDLLForms/Personnel.Designer.cs
+++ WaveAttitudeDLL/WaveAttitudeDLLForms/Personnel.Designer.cs
@@ -40,9 +40,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.personnelBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.waveattitudeDataSet1)).BeginInit();
@@ -123,26 +124,17 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(112, 211);
+            this.button2.Location = new System.Drawing.Point(189, 211);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 30);
             this.button2.TabIndex = 3;
             this.button2.Text = "Nouveau";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(212, 211);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 30);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Rechercher";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(312, 211);
+            this.button4.Location = new System.Drawing.Point(356, 211);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(100, 30);
             this.button4.TabIndex = 5;
@@ -159,14 +151,36 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "id_personnel",
+            "nom_personnel",
+            "prenom_personnel",
+            "age_personnel"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 61);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(170, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // Personnel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(904, 322);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -195,8 +209,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- WaveAttitudeDLL/WaveAttitudeDLLForms/Personnel.cs
+++ WaveAttitudeDLL/WaveAttitudeDLLForms/Personnel.cs
@@ -38,16 +38,55 @@ namespace WaveAttitudeDLLForms
             Accueil frm = new Accueil();
             frm.Show();
         }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button5_Click(object sender, EventArgs e)
         {
             scb = new MySqlCommandBuilder(sda);
             sda.Update(dt);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "id_personnel")
+            { 
+                MySqlConnection conn = new MySqlConnection("server = localhost; user id = root; database = waveattitude");
+                string textreq = "SELECT id_personnel, nom_personnel, prenom_personnel, age_personnel FROM personnel WHERE id_personnel like'"+ textBox1.Text +"%'";
+                sda = new MySqlDataAdapter(textreq, conn);
+                dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (comboBox2.Text == "nom_personnel")
+            {
+                MySqlConnection conn = new MySqlConnection("server = localhost; user id = root; database = waveattitude");
+                string textreq = "SELECT id_personnel, nom_personnel, prenom_personnel, age_personnel FROM personnel WHERE nom_personnel like'" + textBox1.Text + "%'";
+                sda = new MySqlDataAdapter(textreq, conn);
+                dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (comboBox2.Text == "prenom_personnel")
+            {
+                MySqlConnection conn = new MySqlConnection("server = localhost; user id = root; database = waveattitude");
+                string textreq = "SELECT id_personnel, nom_personnel, prenom_personnel, age_personnel FROM personnel WHERE prenom_personnel like'" + textBox1.Text + "%'";
+                sda = new MySqlDataAdapter(textreq, conn);
+                dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else if (comboBox2.Text == "age_personnel")
+            {
+                MySqlConnection conn = new MySqlConnection("server = localhost; user id = root; database = waveattitude");
+                string textreq = "SELECT id_personnel, nom_personnel, prenom_personnel, age_personnel FROM personnel WHERE age_personnel like'" + textBox1.Text + "%'";
+                sda = new MySqlDataAdapter(textreq, conn);
+                dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
         }
     }
 }
